ff8e8d040712fa5ca76ee0ed7568da5c7100df13
ahyiru <ah.yiru@gmail.com>
2020-04-22 11:51:38 +0800

    test: husky

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,6 +2,6 @@ module.exports={
   hooks:{
     'pre-commit':'npm run lint && npm run test',
     // 'pre-push':'npm run test',
-    // 'commit-msg':'commitlint -E HUSKY_GIT_PARAMS',
+    'commit-msg':'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
